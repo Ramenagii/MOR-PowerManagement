6200e9e7073ec93ed883f5bb123fbe2581c12651
Ramenagii <ramenroxaslorenzo15@gmail.com>
2026-09-26 15:23:58 +0800

    Restore test suite in repo (workspace removal is local-only)

---
+++ Mor.PowerManagement/tests/Application.FunctionalTests/TodoItems/Commands/CreateTodoItemTests.cs
@@ -0,0 +1,46 @@
+﻿using Mor.PowerManagement.Application.Common.Exceptions;
+using Mor.PowerManagement.Application.TodoItems.Commands.CreateTodoItem;
+using Mor.PowerManagement.Application.TodoLists.Commands.CreateTodoList;
+using Mor.PowerManagement.Domain.Entities;
+
+namespace Mor.PowerManagement.Application.FunctionalTests.TodoItems.Commands;
+
+public class CreateTodoItemTests : TestBase
+{
+    [Test]
+    public async Task ShouldRequireMinimumFields()
+    {
+        var command = new CreateTodoItemCommand();
+
+        await Should.ThrowAsync<ValidationException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldCreateTodoItem()
+    {
+        var userId = await TestApp.RunAsDefaultUserAsync();
+
+        var listId = await TestApp.SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        var command = new CreateTodoItemCommand
+        {
+            ListId = listId,
+            Title = "Tasks"
+        };
+
+        var itemId = await TestApp.SendAsync(command);
+
+        var item = await TestApp.FindAsync<TodoItem>(itemId);
+
+        item.ShouldNotBeNull();
+        item!.ListId.ShouldBe(command.ListId);
+        item.Title.ShouldBe(command.Title);
+        item.CreatedBy.ShouldBe(userId);
+        item.Created.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+        item.LastModifiedBy.ShouldBe(userId);
+        item.LastModified.ShouldBe(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+}
---
+++ Mor.PowerManagement/tests/Web.AcceptanceTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Mor.PowerManagement.Web.AcceptanceTests.Pages;
+global using Mor.PowerManagement.Shared;
+global using Microsoft.Playwright;
+global using Reqnroll;
+global using Reqnroll.BoDi;
+global using Shouldly;
